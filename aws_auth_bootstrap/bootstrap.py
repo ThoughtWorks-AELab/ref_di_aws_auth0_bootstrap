e5cd1d35bcea9e8ac424d49c5d11bd8a06e41441
Daniel Somerfield <dsomerfi@thoughtworks.com>
2017-05-17 08:02:03 -0700

    [#418] Separated individual example config.

--- aws_auth_bootstrap/bootstrap.py
+++ aws_auth_bootstrap/bootstrap.py
@@ -6,43 +6,6 @@ from aws_auth_bootstrap.builders.auth0tools import Auth0Builder
 
 resource_package = __name__
 
-sso_config = {
-    "project_name": "reference-implementation",
-    "saml_provider_name": "auth0-preproduction",
-    "idp": {
-        "domain": os.environ['AUTH0_HOST'] + ".auth0.com",
-        "client_id": os.environ['AUTH0_CLIENT_ID'],
-        "client_secret": os.environ['AUTH0_CLIENT_SECRET'],
-        "github_client_id": os.environ['GITHUB_CLIENT_ID'],
-        "github_client_secret": os.environ['GITHUB_CLIENT_SECRET']
-    },
-    "accounts": [
-        {
-            "name": "preproduction",
-            "region": "us-west-2",
-            "aws_account_number": os.environ['PREPROD_AWS_ACCOUNT_NUMBER'],
-            "aws_access_key_id": os.environ['PREPROD_AWS_ACCESS_ID'],
-            "aws_secret_access_key": os.environ['PREPROD_SECRET_ACCESS_KEY'],
-            "terraform_dir": 'preproduction',
-            "role_mapping": {
-                "poweruser": ["dev_admin"],
-                "readonly": ["infra_reader"]
-            }
-        },
-        # {
-        #     "name": "production",
-        #     "aws_account_number": os.environ['PROD_AWS_ACCOUNT_NUMBER'],
-        #     "aws_access_key_id": os.environ['PROD_AWS_ACCESS_ID'],
-        #     "aws_secret_access_key": os.environ['PROD_SECRET_ACCESS_KEY'],
-        #     "terraform": pkg_resources.resource_string(resource_package, 'resources/production')
-        # }
-    ],
-    "roles": [
-        ("ThoughtWorks-AELab/dev_admin", "dev_admin"),
-        ("ThoughtWorks-AELab/infra_reader", "infra_reader")
-    ]
-}
-
 
 class Bootstrap:
     def __init__(self, environ=os.environ):
@@ -52,9 +15,9 @@ class Bootstrap:
     def run(self, config):
         auth0builder = Auth0Builder(config["idp"])
 
-        auth0builder.deploy_rules(sso_config['project_name'], {
-            "saml_provider_name": sso_config['saml_provider_name'],
-            "roles": sso_config['roles']
+        auth0builder.deploy_rules(config['project_name'], {
+            "saml_provider_name": config['saml_provider_name'],
+            "roles": config['roles']
         })
 
         accounts = config["accounts"]
@@ -67,7 +30,7 @@ class Bootstrap:
             self.build_policies(
                 account['terraform_dir'],
                 account, config["project_name"],
-                sso_config['saml_provider_name'],
+                config['saml_provider_name'],
                 idp['domain']
             )
 
@@ -115,5 +78,3 @@ class Bootstrap:
     def build_role_list(self, role_array):
         return ", ".join(role_array)
 
-
-Bootstrap().run(sso_config)
---
+++ aws_auth_bootstrap/reference-implementation-bootstrap.py
@@ -0,0 +1,42 @@
+import os
+
+from aws_auth_bootstrap.bootstrap import Bootstrap
+
+sso_config = {
+    "project_name": "reference-implementation",
+    "saml_provider_name": "auth0-preproduction",
+    "idp": {
+        "domain": os.environ['AUTH0_HOST'] + ".auth0.com",
+        "client_id": os.environ['AUTH0_CLIENT_ID'],
+        "client_secret": os.environ['AUTH0_CLIENT_SECRET'],
+        "github_client_id": os.environ['GITHUB_CLIENT_ID'],
+        "github_client_secret": os.environ['GITHUB_CLIENT_SECRET']
+    },
+    "accounts": [
+        {
+            "name": "preproduction",
+            "region": "us-west-2",
+            "aws_account_number": os.environ['PREPROD_AWS_ACCOUNT_NUMBER'],
+            "aws_access_key_id": os.environ['PREPROD_AWS_ACCESS_ID'],
+            "aws_secret_access_key": os.environ['PREPROD_SECRET_ACCESS_KEY'],
+            "terraform_dir": 'preproduction',
+            "role_mapping": {
+                "poweruser": ["dev_admin"],
+                "readonly": ["infra_reader"]
+            }
+        },
+        # {
+        #     "name": "production",
+        #     "aws_account_number": os.environ['PROD_AWS_ACCOUNT_NUMBER'],
+        #     "aws_access_key_id": os.environ['PROD_AWS_ACCESS_ID'],
+        #     "aws_secret_access_key": os.environ['PROD_SECRET_ACCESS_KEY'],
+        #     "terraform": pkg_resources.resource_string(resource_package, 'resources/production')
+        # }
+    ],
+    "roles": [
+        ("ThoughtWorks-AELab/dev_admin", "dev_admin"),
+        ("ThoughtWorks-AELab/infra_reader", "infra_reader")
+    ]
+}
+
+Bootstrap().run(sso_config)
